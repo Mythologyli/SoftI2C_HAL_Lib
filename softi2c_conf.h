3f6627255410641aa26a077891e2e87c20cbde37
lxbpxylps@126.com <lxbpxylps@126.com>
2021-10-12 23:33:44 +0800

    Add file

---
+++ softi2c_conf.h
@@ -0,0 +1,22 @@
+/**
+ * @file    softi2c_conf.h
+ * @author  Myth
+ * @version 0.1
+ * @date    2021.10.12
+ * @brief   STM32 SoftI2C Library Config File
+ */
+
+#ifndef __SOFTI2C_CONF_H
+#define __SOFTI2C_CONF_H
+
+// Set your HAL Library here.
+
+#include "stm32f1xx_hal.h"
+
+// Set your owm Delay_us function here.
+
+#include "systick.h"
+
+#define SoftI2C_Delay_us(__time__) Delay_us(__time__)
+
+#endif
